b91b67a781a45355e31967c70f5cbc868bf7b492
Mateusz <mateuszkrolik87@gmail.com>
2024-09-29 20:37:44 +0200

    22_Docker_Compose-Running_Eureka_Naming_Server

--- data.sql
+++ data.sql
@@ -2,4 +2,4 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple) VALUES
                                                                                         (gen_random_uuid(), 'USD', 'PLN', 3.83),
                                                                                         (gen_random_uuid(), 'EUR', 'PLN', 4.28),
-                                                                                        (gen_random_uuid(), 'GBP', 'INR', 5.12);
+                                                                                        (gen_random_uuid(), 'GBP', 'PLN', 5.12);
