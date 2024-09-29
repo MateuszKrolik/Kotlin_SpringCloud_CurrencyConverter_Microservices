3a8191d62d2f77990612436d67637df8da5d1580
Mateusz <mateuszkrolik87@gmail.com>
2024-09-29 14:21:51 +0200

    10_Setting_up_Eureka_Naming_Server

---
+++ naming-server/src/test/kotlin/com/mateusz/microservices/namingserver/NamingServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mateusz.microservices.namingserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
