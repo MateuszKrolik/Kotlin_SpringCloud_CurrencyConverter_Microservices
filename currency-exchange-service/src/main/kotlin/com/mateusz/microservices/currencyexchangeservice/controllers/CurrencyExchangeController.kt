5d6732401a0e7e6c7785184d8aa07a87bc0805cb
Mateusz <mateuszkrolik87@gmail.com>
2024-09-29 09:31:01 +0200

    4_Setting_up_Dynamic_Port_Response_of_CurrencyExchange

--- currency-exchange-service/src/main/kotlin/com/mateusz/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.kt
+++ currency-exchange-service/src/main/kotlin/com/mateusz/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.kt
@@ -1,6 +1,7 @@
 package com.mateusz.microservices.currencyexchangeservice.controllers
 
 import com.mateusz.microservices.currencyexchangeservice.models.CurrencyExchange
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.PathVariable
@@ -8,9 +9,13 @@ import java.math.BigDecimal
 import java.util.*
 
 @RestController
-class CurrencyExchangeController {
+class CurrencyExchangeController(private var environment: Environment) {
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     fun retrieveExchangeValue(@PathVariable from: String, @PathVariable to:String): CurrencyExchange {
-        return CurrencyExchange( UUID.randomUUID(), from, to, BigDecimal.valueOf(50))
+        val currencyExchange = CurrencyExchange(UUID.randomUUID(), from, to, BigDecimal.valueOf(50))
+        val port = environment.getProperty("local.server.port")
+        currencyExchange.environment = port!!
+        return currencyExchange
     }
 }
--- currency-exchange-service/src/main/kotlin/com/mateusz/microservices/currencyexchangeservice/models/CurrencyExchange.kt
+++ currency-exchange-service/src/main/kotlin/com/mateusz/microservices/currencyexchangeservice/models/CurrencyExchange.kt
@@ -5,13 +5,14 @@ import java.util.*
 
 class CurrencyExchange {
     var id: UUID? = null
-    var from: String? = null
-    var to: String? = null
-    var conversionMultiple: BigDecimal? = null
+    var from: String = ""
+    var to: String = ""
+    var conversionMultiple: BigDecimal = BigDecimal(0)
+    var environment: String = ""
 
     constructor()
 
-    constructor(id: UUID?, from: String?, to: String?, conversionMultiple: BigDecimal?) {
+    constructor(id: UUID?, from: String, to: String, conversionMultiple: BigDecimal) {
         this.id = id
         this.from = from
         this.to = to
