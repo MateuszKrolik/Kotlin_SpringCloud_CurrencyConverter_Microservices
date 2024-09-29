b5d8c562986ea464d7af743254cb640feb48ad56
Mateusz <mateuszkrolik87@gmail.com>
2024-09-29 17:39:08 +0200

    17_Resilience4j-Implementing_Circuit Breaker_Features

--- currency-exchange-service/src/main/kotlin/com/mateusz/microservices/currencyexchangeservice/controllers/CircuitBreakerController.kt
+++ currency-exchange-service/src/main/kotlin/com/mateusz/microservices/currencyexchangeservice/controllers/CircuitBreakerController.kt
@@ -1,6 +1,7 @@
 package com.mateusz.microservices.currencyexchangeservice.controllers
 
-import io.github.resilience4j.retry.annotation.Retry
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
+// import io.github.resilience4j.retry.annotation.Retry
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
@@ -8,13 +9,17 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
 import java.lang.Exception
 
+// spam requests for circuit-breaker testing purposes:
+// "watch -n 0.1 curl http://localhost:8000/sample-api"
+
 @RestController
 class CircuitBreakerController {
 
     private val logger: Logger = LoggerFactory.getLogger(CircuitBreakerController::class.java)
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardCodedResponse")
+    // @Retry(name = "sample-api", fallbackMethod = "hardCodedResponse")
+    @CircuitBreaker(name = "default", fallbackMethod = "hardCodedResponse")
     fun sampleApi():String {
         logger.info("Sample Api Call Received")
         val forEntity = RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String::class.java)
