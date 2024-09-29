d8dd8a3f6d80381befd922ad17587e3957fc20ee
Mateusz <mateuszkrolik87@gmail.com>
2024-09-29 16:48:13 +0200

    15_Implementing_Spring_Cloud_Gateway_Logging_Filter

---
+++ api-gateway/src/main/kotlin/com/mateusz/microservices/apigateway/filters/LoggingFilter.kt
@@ -0,0 +1,20 @@
+package com.mateusz.microservices.apigateway.filters
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.cloud.gateway.filter.GatewayFilterChain
+import org.springframework.cloud.gateway.filter.GlobalFilter
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+
+@Component
+class LoggingFilter: GlobalFilter {
+
+    private val logger: Logger = LoggerFactory.getLogger(LoggingFilter::class.java)
+
+    override fun filter(exchange: ServerWebExchange?, chain: GatewayFilterChain?): Mono<Void> {
+        logger.info("Path of the request received -> {}", exchange?.request?.path)
+        return chain!!.filter(exchange)
+    }
+}
